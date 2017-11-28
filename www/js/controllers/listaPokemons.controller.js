7de66d458134515a9b0b3e964617b78a5f651cc9
Lucas Bruzzi <lucasbruzzi@phari.solutions>
2017-11-28 00:03:21 -0200

    Barra de pesquisa implementada!

--- www/js/controllers/listaPokemons.controller.js
+++ www/js/controllers/listaPokemons.controller.js
@@ -1,14 +1,18 @@
 angular.module('starter').controller('ListaPokemonsController', function($scope, PokedexService){
+  
   PokedexService.obterPokemons().then(function(dados){
     $scope.listaPokemons = dados.pokemon;
     $scope.listaPokemons.forEach(function(element){
       element.id = parseInt(element.resource_uri.split('/')[3]);
     })
+
     $scope.listaPokemons.sort(function (a, b){
       if (a.id > b.id) return 1;
       else if (a.id < b.id) return -1;
       else return 0;
     });
+
+    $scope.searchInput = null;
   })
 });
 

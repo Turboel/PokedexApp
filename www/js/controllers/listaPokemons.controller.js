c826da04b3c9db3ddd26f497cbab6e34975b840b
Francisco Neves <franciscofabneves@gmail.com>
2017-11-27 22:46:56 -0200

    Numeros ok ordenacao a partir dos numeros !ok :(

--- www/js/controllers/listaPokemons.controller.js
+++ www/js/controllers/listaPokemons.controller.js
@@ -1,5 +1,13 @@
 angular.module('starter').controller('ListaPokemonsController', function($scope, PokedexService){
   PokedexService.obterPokemons().then(function(dados){
     $scope.listaPokemons = dados.pokemon;
+    $scope.listaPokemons.forEach(function(element){
+      element.id = parseInt(element.resource_uri.split('/')[3]);
+    })
+    $scope.sort(function (a, b){
+      if (a.num > b.num) return 1;
+      else if (a.num < b.num) return -1;
+      else return 0;
+    });
   })
 });

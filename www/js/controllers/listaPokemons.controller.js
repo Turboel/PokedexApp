51edf613a4ec38ea93b5fd0b0ddcc2bd98cc2f88
Francisco Neves <franciscofabneves@gmail.com>
2017-11-27 22:50:37 -0200

    ORDENACAO

--- www/js/controllers/listaPokemons.controller.js
+++ www/js/controllers/listaPokemons.controller.js
@@ -4,9 +4,9 @@ angular.module('starter').controller('ListaPokemonsController', function($scope,
     $scope.listaPokemons.forEach(function(element){
       element.id = parseInt(element.resource_uri.split('/')[3]);
     })
-    $scope.sort(function (a, b){
-      if (a.num > b.num) return 1;
-      else if (a.num < b.num) return -1;
+    $scope.listaPokemons.sort(function (a, b){
+      if (a.id > b.id) return 1;
+      else if (a.id < b.id) return -1;
       else return 0;
     });
   })
